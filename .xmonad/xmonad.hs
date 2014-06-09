ac3169b1bf1ef9c98aef6da26cbee2066785e601
Conner McDaniel <connermcd@gmail.com>
2014-06-09 00:06:26 -0500

    Put daemons in systemd where they belong.

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -36,7 +36,7 @@ main :: IO ()
 main = xmonad =<< statusBar cmd pp kb conf
   where
     uhook = withUrgencyHookC NoUrgencyHook urgentConfig
-    cmd = "xmobar -x0"
+    cmd = ""
     --cmd = "bash -c \"tee >(xmobar -x0) | xmobar -x1 | xmobar -x2\""
     pp = customPP
     kb = toggleStrutsKey
